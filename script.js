62fd99b5dfa1df94b943dda57e6c595ed62dac88
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-24 15:16:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,9 +14,35 @@ class Person {
 	}
 }
 
-class Student extends Person {}
+class Student extends Person {
+	constructor(name, age){
+		this._name=name;
+		this._age=age;
+	}
 
-class Teacher extends Person {}
+	get name(){
+		return this._name;
+	}
+
+	set age(age){
+		this._age=age;
+	}
+}
+
+class Teacher extends Person {
+	constructor(name, age){
+		this._name=name;
+		this._age=age;
+	}
+
+	get name(){
+		return this._name;
+	}
+
+	set age(age){
+		this._age=age;
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
